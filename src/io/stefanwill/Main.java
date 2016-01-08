080bc396596f18b3b3410990fe50cd811dcc95f8
Stefan <stefan-11@gmx.de>
2016-01-08 15:26:28 +0100

    first working version

--- src/io/stefanwill/Main.java
+++ src/io/stefanwill/Main.java
@@ -2,6 +2,13 @@ package io.stefanwill;
 
 import io.stefanwill.jcrop.JCrop;
 
+import java.awt.image.BufferedImage; // I think
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.imageio.*;
+import java.io.*;
+
 public class Main {
 
 
@@ -12,6 +19,39 @@ public class Main {
 
         JCrop cropper = new JCrop();
         cropper.sayHello();
+
+
+        //load an image into a buffered image
+        //SW_HI_2013-06-30_19-05-10_eos450d_IMG_1314.jpg
+
+        BufferedImage img = null;
+        BufferedImage targetImg = null;
+
+        try {
+            img = ImageIO.read(new File("SW_HI_2013-06-30_19-05-10_eos450d_IMG_1314.jpg"));
+
+            System.out.println("got it");
+
+            Rectangle croppedRect = new Rectangle(500, 500);
+
+            targetImg = cropper.crop(img, croppedRect);
+
+            System.out.println("cropped");
+
+        } catch (IOException e) {
+            System.out.println("IOException aufgetreten");
+        }
+
+
+
+        //write the image to a file
+        try {
+
+            File outputfile = new File("saved.jpg");
+            ImageIO.write(targetImg, "jpg", outputfile);
+        } catch (IOException e) {
+            System.out.println("IOException aufgetreten während dem schreiben");
+        }
 
     }
 }
--- src/io/stefanwill/jcrop/JCrop.java
+++ src/io/stefanwill/jcrop/JCrop.java
@@ -21,7 +21,8 @@ public class JCrop {
 
     public BufferedImage crop(BufferedImage src, Rectangle rect)
     {
-        BufferedImage dest = new BufferedImage((int) rect.getWidth(), (int) rect.getHeight(), BufferedImage.TYPE_4BYTE_ABGR_PRE);
+        //BufferedImage dest = new BufferedImage((int) rect.getWidth(), (int) rect.getHeight(), BufferedImage.TYPE_4BYTE_ABGR_PRE);
+        BufferedImage dest = new BufferedImage((int) rect.getWidth(), (int) rect.getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics g = dest.getGraphics();
         g.drawImage(src, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), (int) rect.getX(), (int) rect.getY(), (int) rect.getX() + (int) rect.getWidth(), (int) rect.getY() + (int) rect.getHeight(), null);
         g.dispose();
