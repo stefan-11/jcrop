04b9ff9afd2c4552be538f9c01dd085a6ad19f92
Stefan <stefan-11@gmx.de>
2016-01-08 14:57:46 +0100

    initial commit

---
+++ src/io/stefanwill/Main.java
@@ -0,0 +1,17 @@
+package io.stefanwill;
+
+import io.stefanwill.jcrop.JCrop;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("hello world!");
+
+        JCrop cropper = new JCrop();
+        cropper.sayHello();
+
+    }
+}
---
+++ src/io/stefanwill/jcrop/JCrop.java
@@ -0,0 +1,31 @@
+/**
+ * Created by stefan on 08.01.16.
+ */
+
+package io.stefanwill.jcrop;
+
+import java.awt.image.BufferedImage; // I think
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class JCrop {
+
+    public void sayHello(){
+
+        System.out.println("hello");
+
+    }
+
+
+    public BufferedImage crop(BufferedImage src, Rectangle rect)
+    {
+        BufferedImage dest = new BufferedImage((int) rect.getWidth(), (int) rect.getHeight(), BufferedImage.TYPE_4BYTE_ABGR_PRE);
+        Graphics g = dest.getGraphics();
+        g.drawImage(src, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), (int) rect.getX(), (int) rect.getY(), (int) rect.getX() + (int) rect.getWidth(), (int) rect.getY() + (int) rect.getHeight(), null);
+        g.dispose();
+        return dest;
+    }
+
+}
