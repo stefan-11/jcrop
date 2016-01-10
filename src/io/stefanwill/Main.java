95e56cd1f01507d05cd645565d816a005193bcf1
Stefan <stefan-11@gmx.de>
2016-01-10 13:38:05 +0100

    crop and resizing working

--- src/io/stefanwill/Main.java
+++ src/io/stefanwill/Main.java
@@ -15,35 +15,34 @@ public class Main {
 	    // write your code here
         System.out.println("hello world!");
 
-        JCrop cropper = new JCrop();
-        cropper.sayHello();
-
-
-        //load an image into a buffered image
-        //SW_HI_2013-06-30_19-05-10_eos450d_IMG_1314.jpg
+        Main main = new Main();
 
         BufferedImage img = null;
         BufferedImage targetImg = null;
 
+
+
         //load it
         try {
-            img = ImageIO.read(new File("landscape.jpg"));
+            //img = ImageIO.read(new File("landscape.jpg"));
+            //img = ImageIO.read(new File("pano.jpg"));
+            img = ImageIO.read(new File("portrait.jpg"));
 
-            System.out.println("got it");
+            System.out.println("loaded");
 
         } catch (IOException e) {
-            System.out.println("IOException aufgetreten");
+            System.out.println("Loading failed.");
         }
 
 
-
+/*
         //resize it
         try {
             System.out.println("resizing...");
             img = JCrop.resize(img, 2848, 2848);
             System.out.println("done");
         } catch (Exception e) {
-            System.out.println("Exception aufgetreten");
+            System.out.println("resizing failed.");
             e.printStackTrace();
         }
 
@@ -53,15 +52,19 @@ public class Main {
 
             Rectangle croppedRect = new Rectangle(500, 500);
 
-            targetImg = cropper.crop(img, croppedRect);
+            targetImg = JCrop.crop(img, croppedRect, 2100, 1400);
 
             System.out.println("cropped");
 
         } catch (Exception e) {
-            System.out.println("Exception aufgetreten");
+            System.out.println("cropping failed.");
         }
 
+*/
 
+
+
+        targetImg = main.createThumbnail(img);
 
 
         //write the image to a file
@@ -72,12 +75,114 @@ public class Main {
             }
 
             System.out.println("saving cropped image");
-            File outputfile = new File("saved-landscape.jpg");
+            File outputfile = new File("output.jpg");
             ImageIO.write(targetImg, "jpg", outputfile);
             System.out.println("done");
         } catch (IOException e) {
-            System.out.println("IOException aufgetreten während dem schreiben");
+            System.out.println("writing failed.");
         }
+
+
+
+
+
+    }
+
+
+    public BufferedImage createThumbnail(BufferedImage img){
+
+        //widht/height of the original image
+        int imageWidth = img.getWidth();
+        int imageHeight = img.getHeight();
+
+        //width/height of the thumbnail (thumbnail will be landscape even for portrait images)
+        int thumbHeight = 200;
+        int thumbWidth = 300;
+
+        //widht/height of the (smaller) resized image
+        double scalingFactor = 0.5;
+        int resizedHeigth = 0;
+        int resizedWidth = 0;
+
+
+
+        System.out.println("width");
+        System.out.println(imageWidth);
+        System.out.println("height");
+        System.out.println(imageHeight);
+
+
+        //if height is bigger than width we have a portrait image
+        //calculate the scaling factor depending on the short edge of the image
+        if (imageHeight > imageWidth){
+            System.out.println("image is portrait");
+
+            System.out.println("thumbWidth: ");
+            System.out.println(thumbWidth);
+            System.out.println("imageWidth: ");
+            System.out.println(imageWidth);
+            scalingFactor = (double) thumbWidth / (double) imageWidth;
+
+        } else {
+            System.out.println("image is landscape");
+
+            System.out.println("thumbHeight: ");
+            System.out.println(thumbHeight);
+            System.out.println("imageHeight: ");
+            System.out.println(imageHeight);
+            scalingFactor = (double) thumbHeight / (double) imageHeight;
+
+        }
+        System.out.println("scalingFactor");
+        System.out.println(scalingFactor);
+
+        //recalculate height/width of the thumbnail (should be same as thumbWidth/thumbHeight)
+        resizedHeigth = (int) (imageHeight * scalingFactor);
+        System.out.println("resizedHeight:");
+        System.out.println(resizedHeigth);
+
+        resizedWidth = (int) (imageWidth * scalingFactor);
+        System.out.println("resizedWidth:");
+        System.out.println(resizedWidth);
+
+        //resize it
+        try {
+            System.out.println("resizing...");
+            //now resize the image
+            img = JCrop.resize(img, resizedWidth, resizedHeigth);
+            System.out.println("done");
+        } catch (Exception e) {
+            System.out.println("Exception aufgetreten");
+            e.printStackTrace();
+        }
+
+
+        //crop it
+        try {
+
+            Rectangle croppedRect = new Rectangle(thumbWidth, thumbHeight);
+            System.out.println("croppedRect");
+            System.out.println(croppedRect);
+
+            int originX = (resizedWidth / 2) - (thumbWidth / 2);
+            System.out.println("originX");
+            System.out.println(originX);
+
+            int originY = (resizedHeigth / 2) - (thumbHeight / 2);
+            System.out.println("originY");
+            System.out.println(originY);
+
+            //now crop the image
+            img = JCrop.crop(img, croppedRect, originX, originY);
+
+            System.out.println("cropped");
+
+        } catch (Exception e) {
+            System.out.println("Exception aufgetreten");
+        }
+
+
+        return img;
 
     }
 }
--- src/io/stefanwill/jcrop/JCrop.java
+++ src/io/stefanwill/jcrop/JCrop.java
@@ -10,17 +10,25 @@ import java.awt.image.BufferedImage; // I think
 
 public class JCrop {
 
-    public void sayHello() {
+    public static BufferedImage crop(BufferedImage src, Rectangle rect) {
 
-        System.out.println("hello");
-
-    }
-
-
-    public BufferedImage crop(BufferedImage src, Rectangle rect) {
+        //crop with default origin
+        BufferedImage dest = JCrop.crop(src, rect, 0, 0);
+        /*
         BufferedImage dest = new BufferedImage((int) rect.getWidth(), (int) rect.getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics g = dest.getGraphics();
         g.drawImage(src, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), (int) rect.getX(), (int) rect.getY(), (int) rect.getX() + (int) rect.getWidth(), (int) rect.getY() + (int) rect.getHeight(), null);
+        //g.drawImage(src, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), 1000, 1000, (int) rect.getX() + (int) rect.getWidth(), (int) rect.getY() + (int) rect.getHeight(), null);
+        g.dispose();
+        */
+        return dest;
+    }
+
+    public static BufferedImage crop(BufferedImage src, Rectangle rect, int originX, int originY) {
+        BufferedImage dest = new BufferedImage((int) rect.getWidth(), (int) rect.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics g = dest.getGraphics();
+        //g.drawImage(src, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), (int) rect.getX(), (int) rect.getY(), (int) rect.getX() + (int) rect.getWidth(), (int) rect.getY() + (int) rect.getHeight(), null);
+        g.drawImage(src, 0, 0, (int) rect.getWidth(), (int) rect.getHeight(), originX, originY, (int) rect.getX() + (int) (originX + rect.getWidth()), (int) rect.getY() + (int) (originY + rect.getHeight()), null);
         g.dispose();
         return dest;
     }
@@ -34,6 +42,11 @@ public class JCrop {
         g2d.dispose();
 
         return dimg;
+    }
+
+    public static boolean isPortrait(BufferedImage img){
+
+        return false;
     }
 
 }
